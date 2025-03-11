832690e03508ed79f8c57ddfe7c366300e0bc9b3
maks2001916 <mamonov.maksim.20003@gmail.com>
2025-03-11 21:14:32 +0500

    класс Colors адаптирован под android.graphics.Color

--- oriedita-data/src/main/java/com/example/oriedita_data/Colors.java
+++ oriedita-data/src/main/java/com/example/oriedita_data/Colors.java
@@ -1,56 +1,56 @@
 package com.example.oriedita_data;
 
-import java.awt.Color;
+import android.graphics.Color;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Colors {
-    public static final Color GRID_LINE_DARK = new Color(50, 50, 50);
-    public static final Color GRID_SCALE_DARK = new Color(17, 75, 10);
+    public static final int GRID_LINE_DARK = Color.rgb(50, 50, 50);
+    public static final int GRID_SCALE_DARK = Color.rgb(17, 75, 10);
 
-    public static final Color GRID_LINE = new Color(230, 230, 230);
-    public static final Color GRID_SCALE = new Color(180, 200, 180);
+    public static final int GRID_LINE = Color.rgb(230, 230, 230);
+    public static final int GRID_SCALE = Color.rgb(180, 200, 180);
 
-    public static final Color FIGURE_FRONT_DARK = new Color(129, 15, 94);
-    public static final Color FIGURE_BACK_DARK = new Color(89, 89, 89);
+    public static final int FIGURE_FRONT_DARK = Color.rgb(129, 15, 94);
+    public static final int FIGURE_BACK_DARK = Color.rgb(89, 89, 89);
 
-    public static final Color FIGURE_FRONT = new Color(255, 255, 50);
-    public static final Color FIGURE_BACK = new Color(233, 233, 233);
+    public static final int FIGURE_FRONT = Color.rgb(255, 255, 50);
+    public static final int FIGURE_BACK = Color.rgb(233, 233, 233);
 
-    public static final Color INVALID_INPUT = new Color(255, 153, 153);
-    public static final Color INVALID_INPUT_DARK = new Color(160, 60, 80);
+    public static final int INVALID_INPUT = Color.rgb(255, 153, 153);
+    public static final int INVALID_INPUT_DARK = Color.rgb(160, 60, 80);
 
-    private static final Map<Color, Color> colorMap;
-    private static final Map<Color, Color> darkColorMap;
+    private static final Map<Integer, Integer> colorMap;
+    private static final Map<Integer, Integer> darkColorMap;
 
     static {
         colorMap = new HashMap<>();
         darkColorMap = new HashMap<>();
 
-        add(Color.black, Color.black, new Color(210, 210, 210));
-        add(Color.white, Color.white, new Color(37, 37, 37));
-        add(Color.red, Color.red, new Color(229, 115, 115));
-        add(Color.blue, Color.blue, new Color(33, 150, 243));
-        add(Color.cyan, Color.cyan, new Color(0, 100, 100));
-        add(Color.magenta, Color.magenta, new Color(100, 0, 100));
-        add(Color.green, Color.green, new Color(0, 100, 0));
-        add(new Color(150, 150, 150), new Color(150, 150, 150), new Color(50, 50, 50));
-        add(new Color(255, 0, 0, 75), new Color(255, 0, 0, 75), new Color(255, 0, 0, 75));
-        add(new Color(230, 230, 230), new Color(230, 230, 230), new Color(54, 54, 54));
-        add(new Color(162, 162, 162), new Color(162, 162, 162), new Color(120, 120, 120)); //placeholder
-        add(Color.gray, new Color(128,128,128,128), new Color(128,128,128,128));
-        add(Color.yellow, Color.magenta, Color.yellow);
+        add(Color.BLACK, Color.BLACK, Color.rgb(210, 210, 210));
+        add(Color.WHITE, Color.WHITE, Color.rgb(37, 37, 37));
+        add(Color.RED, Color.RED, Color.rgb(229, 115, 115));
+        add(Color.BLUE, Color.BLUE, Color.rgb(33, 150, 243));
+        add(Color.CYAN, Color.CYAN, Color.rgb(0, 100, 100));
+        add(Color.MAGENTA, Color.MAGENTA, Color.rgb(100, 0, 100));
+        add(Color.GREEN, Color.GREEN, Color.rgb(0, 100, 0));
+        add(Color.rgb(150, 150, 150), Color.rgb(150, 150, 150), Color.rgb(50, 50, 50));
+        add(Color.argb(0, 0, 75, 255), Color.argb(0, 0, 75, 255), Color.argb(0, 0, 75, 255));
+        add(Color.rgb(230, 230, 230), Color.rgb(230, 230, 230), Color.rgb(54, 54, 54));
+        add(Color.rgb(162, 162, 162), Color.rgb(162, 162, 162), Color.rgb(120, 120, 120)); //placeholder
+        add(Color.GRAY, Color.argb(128,128,128,128), Color.argb(128,128,128,128));
+        add(Color.YELLOW, Color.MAGENTA, Color.YELLOW);
         add(INVALID_INPUT, INVALID_INPUT, INVALID_INPUT_DARK);
     }
 
-    private static Map<Color, Color> activeColorMap = colorMap;
+    private static Map<Integer, Integer> activeColorMap = colorMap;
 
-    private static void add(Color color, Color lightColor, Color darkColor) {
+    private static void add(int color, int lightColor, int darkColor) {
         colorMap.put(color, lightColor);
         darkColorMap.put(color, darkColor);
     }
 
-    public static Color get(Color color) {
+    public static int get(int color) {
         return activeColorMap.getOrDefault(color, color);
     }
 
@@ -69,15 +69,20 @@ public class Colors {
      * @param isDark The desired mode for this color to be in.
      * @return Restored color
      */
-    public static Color restore(Color color, boolean isDark) {
-        if (color == null) {
-            return null;
+    public static int restore(int color, boolean isDark) {
+        if (color == Color.TRANSPARENT) {
+            return Color.TRANSPARENT;
         }
 
         if (isDark) {
             return darkColorMap.getOrDefault(color, color);
         }
 
-        return darkColorMap.entrySet().stream().filter(entry -> color.equals(entry.getValue())).map(Map.Entry::getKey).findFirst().orElse(color);
+        for (Map.Entry<Integer, Integer> entry : darkColorMap.entrySet()) {
+            if (entry.getValue() == color) {
+                return entry.getKey();
+            }
+        }
+        return color;
     }
 }
