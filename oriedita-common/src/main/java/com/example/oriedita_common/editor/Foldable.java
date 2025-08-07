5331f2a394c21baaa43ed3fd80db534694fe2190
max <mamonov.maksim.20003@gmail.com>
2025-08-07 18:58:22 +0500

    Foldable адаптирован

--- oriedita-common/src/main/java/com/example/oriedita_common/editor/Foldable.java
+++ oriedita-common/src/main/java/com/example/oriedita_common/editor/Foldable.java
@@ -1,21 +1,53 @@
 package com.example.oriedita_common.editor;
 
-import oriedita.editor.drawing.tools.Camera;
-import origami.crease_pattern.FoldingException;
-import origami.crease_pattern.LineSegmentSet;
-import origami.folding.FoldedFigure;
+import com.example.oriedita_common.editor.drawing.tools.Camera;
+import com.example.oriedita_core.origami.crease_pattern.FoldingException;
+import com.example.oriedita_core.origami.crease_pattern.LineSegmentSet;
+import com.example.oriedita_core.origami.folding.FoldedFigure;
 
+/**
+ * Интерфейс для работы со складыванием оригами в Android
+ * Адаптирован для работы с Jetpack Compose и Android-специфичными компонентами
+ */
 public interface Foldable {
+    
+    /**
+     * Выполняет оценку складывания оригами
+     * @param creasePatternCamera камера для отображения паттерна складок
+     * @param lineSegmentSet набор сегментов линий для складывания
+     * @throws InterruptedException если операция была прервана
+     * @throws FoldingException если произошла ошибка при складывании
+     */
     void folding_estimated(Camera creasePatternCamera, LineSegmentSet lineSegmentSet) throws InterruptedException, FoldingException;
 
+    /**
+     * Устанавливает порядок оценки складывания
+     * @param estimationOrder порядок оценки
+     */
     void setEstimationOrder(FoldedFigure.EstimationOrder estimationOrder);
 
+    /**
+     * Инициализирует оценку складывания
+     */
     void estimated_initialize();
 
+    /**
+     * Получает текстовый результат складывания
+     * @return текстовый результат
+     */
     String getTextResult();
 
+    /**
+     * Устанавливает текстовый результат складывания
+     * @param textResult текстовый результат
+     */
     void setTextResult(String textResult);
 
-    void createTwoColorCreasePattern(Camera camera_of_foldLine_diagram, LineSegmentSet Ss0) throws InterruptedException; //Two-color crease pattern
-
+    /**
+     * Создает двухцветный паттерн складок
+     * @param camera_of_foldLine_diagram камера для диаграммы линий складывания
+     * @param Ss0 набор сегментов линий
+     * @throws InterruptedException если операция была прервана
+     */
+    void createTwoColorCreasePattern(Camera camera_of_foldLine_diagram, LineSegmentSet Ss0) throws InterruptedException;
 }
